30425249f6224cdcf9e871d86fe0025dcee9a669
Ungable <Sarecud@gmail.com>
2024-02-01 22:44:19 +0700

    add TwoCum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine(TwoSum([2,5,5,11], 10));
+
+int[] TwoSum(int[] nums, int target)
+{
+   for (int x = 0; x < nums.Length; x++)
+   {
+      for (int y = x + 1; y < nums.Length; y++)
+      {
+         Console.WriteLine(nums[y]);
+         if (nums[x] + nums[y] == target )
+         {
+            nums = [x,y];
+            return nums;
+         }         
+      }       
+   }
+   return nums;
+}
